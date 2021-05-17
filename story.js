ab0fb0bddf6df27e634698a6fedc37c44d05ce0a
Ahmad1431997 <ahmadabuawed@gmail.com>
2021-05-17 16:19:46 +0300

    Task4 solution

---
+++ story.js
@@ -0,0 +1,5 @@
+let name = "Ahmad";
+let age = 24;
+let hobby= "play video game";
+age +=33;
+console.log(`${name} in 33 years, you will be ${age} years old. By then you'll have decades of ${hobby} practice!`);
